d0993dd954156a7bde182c169d8c3a026dd91640
Craig Edney <caedney@btinternet.com>
2024-09-16 07:53:55 +0100

    feat: Add file based routing

---
+++ app/_layout.tsx
@@ -0,0 +1,21 @@
+import { Stack } from 'expo-router';
+
+export default function Layout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: 'Shopping List' }} />
+      <Stack.Screen
+        name="counter"
+        options={{
+          title: 'Counter',
+          presentation: 'modal',
+          animation: 'slide_from_bottom', // overrides animation on Android
+        }}
+      />
+      <Stack.Screen
+        name="idea"
+        options={{ title: 'Idea', presentation: 'modal' }}
+      />
+    </Stack>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
 import { StyleSheet, View } from 'react-native';
-import theme from './theme';
-import ShoppingListItem from './components/ShoppingListItem';
+import theme from '../theme';
+import ShoppingListItem from '../components/ShoppingListItem';
+import { Link } from 'expo-router';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Link href="/counter">Go to counter</Link>
       <ShoppingListItem name="Coffee" />
       <ShoppingListItem name="Tea" completed />
       <ShoppingListItem name="Sugar" completed />
