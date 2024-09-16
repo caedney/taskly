480ef7543952c40ea0ab75f525b52791fe63382b
Craig Edney <caedney@btinternet.com>
2024-09-16 09:05:02 +0100

    feat: Add history screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,44 @@
-import { Stack } from 'expo-router';
+import { Tabs } from 'expo-router';
+import Feather from '@expo/vector-icons/Feather';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import theme from '../theme';
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: 'Shopping List' }} />
-      <Stack.Screen
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: theme.colorCerulean,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Shopping List',
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="list" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
         name="counter"
         options={{
           title: 'Counter',
-          presentation: 'modal',
-          animation: 'slide_from_bottom', // overrides animation on Android
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="clockcircleo" size={size} color={color} />
+          ),
         }}
       />
-      <Stack.Screen
+      <Tabs.Screen
         name="idea"
-        options={{ title: 'Idea', presentation: 'modal' }}
+        options={{
+          title: 'Idea',
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="lightbulb" size={size} color={color} />
+          ),
+        }}
       />
-    </Stack>
+    </Tabs>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,64 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, TextInput, FlatList, View, Text } from 'react-native';
 import theme from '../theme';
 import ShoppingListItem from '../components/ShoppingListItem';
-import { Link } from 'expo-router';
+import React from 'react';
+
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+};
+
+// const initialList: ShoppingListItemType[] = [
+//   { id: '1', name: 'Coffee' },
+//   { id: '2', name: 'Tea' },
+//   { id: '3', name: 'Idea' },
+// ];
+
+// const newList = new Array(1000)
+//   .fill(null)
+//   .map((item, index) => ({ id: index.toString(), name: index.toString() }));
 
 export default function App() {
+  const [value, setValue] = React.useState('');
+  const [shoppingList, setShoppingList] = React.useState<
+    ShoppingListItemType[]
+  >([]);
+
+  const handleSubmit = () => {
+    if (value) {
+      setShoppingList((prevList) => {
+        return [{ id: new Date().toTimeString(), name: value }, ...prevList];
+      });
+      setValue('');
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Link href="/counter">Go to counter</Link>
-      <ShoppingListItem name="Coffee" />
-      <ShoppingListItem name="Tea" completed />
-      <ShoppingListItem name="Sugar" completed />
-    </View>
+    <FlatList
+      data={shoppingList}
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      stickyHeaderIndices={[0]}
+      ListEmptyComponent={() => (
+        <View style={styles.listEmptyContainer}>
+          <Text>Your shopping list is empty</Text>
+        </View>
+      )}
+      ListHeaderComponent={
+        <TextInput
+          style={styles.textInput}
+          placeholder="Coffee"
+          value={value}
+          onChangeText={setValue}
+          returnKeyType="done"
+          onSubmitEditing={handleSubmit}
+        />
+      }
+      renderItem={({ item }) => {
+        // console.log(item);
+        return <ShoppingListItem name={item.name} />;
+      }}
+    />
   );
 }
 
@@ -18,6 +66,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
+    paddingVertical: 16,
+    paddingHorizontal: 12,
+  },
+  contentContainer: {
+    paddingBottom: 32,
+  },
+  listEmptyContainer: {
     justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 18,
+  },
+  textInput: {
+    backgroundColor: theme.colorWhite,
+    borderColor: theme.colorLightGrey,
+    borderWidth: 2,
+    padding: 12,
+    marginHorizontal: 12,
+    marginBottom: 12,
+    fontSize: 18,
+    borderRadius: 50,
+    paddingHorizontal: 20,
   },
 });
